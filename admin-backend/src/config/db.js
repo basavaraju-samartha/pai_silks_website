58ac976d2c1843a13494d3269023d3abef1a42e1
SinchananazreT-tech <sinchanant24@gmail.com>
2025-11-13 22:33:46 +0530

    Update db.js

--- admin-backend/src/config/db.js
+++ admin-backend/src/config/db.js
@@ -2,13 +2,13 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db",
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password:process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'db',
   waitForConnections: true,
   connectionLimit: 10,
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   queueLimit: 0
 });
 
