47dcea75883d5d6b4cd1773554039327bf5f735a
Basavaraju-MN <basavaraju.m.n22@gmail.com>
2025-11-24 21:29:42 +0530

    Update server.js

--- client-backend/src/server.js
+++ client-backend/src/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const customerRoutes = require('./routes/customerRoutes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(
+  cors({
+    origin: true, // Allow any origin (you can replace this with a specific origin)
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
